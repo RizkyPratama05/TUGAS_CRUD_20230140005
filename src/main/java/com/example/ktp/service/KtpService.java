8521393411ce26366d3636ff2d24d0dbca27e0a5
RizkyPratama05 <rizkypp428@gmail.com>
2026-03-14 14:46:56 +0700

    mengisi file KtpService

--- src/main/java/com/example/ktp/service/KtpService.java
+++ src/main/java/com/example/ktp/service/KtpService.java
@@ -1,4 +1,12 @@
 package com.example.ktp.service;
 
-public class KtpService {
+import com.example.ktp.dto.KtpDto;
+import java.util.List;
+
+public interface KtpService {
+    KtpDto addKtp(KtpDto request);
+    List<KtpDto> getAllKtp();
+    KtpDto getKtpById(Integer id);
+    KtpDto updateKtp(Integer id, KtpDto request);
+    void deleteKtp(Integer id);
 }
